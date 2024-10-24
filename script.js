7f0c9949e324fc60c3f929ff344b99c176a2523c
Shamail Ahmad <125307010+Shumail97@users.noreply.github.com>
2024-10-24 22:52:32 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+/** @format */
+
+var checkPalindrome = function (word) {
+  var inputWord = word;
+  var checkString = inputWord.split(" ").join("").toLowerCase();
+  for (var i = 0; i < checkString.length / 2; i++) {
+    if (checkString[i] != checkString[checkString.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+};
+var output = checkPalindrome("Racer");
+console.log(output);
